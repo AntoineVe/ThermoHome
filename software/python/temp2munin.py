a295329480e9f601240e98e298779f94309e63e4
AntoineVe <antoine@van-elstraete.net>
2014-01-17 19:44:03 +0100

    Ajout d'un monitoring par munin

---
+++ software/python/temp2munin.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import serial #Importe la bibliothèque de communication série
+import time #Importe la bibliothèque pour gérer le temps
+import threading
+import http.server
+import socketserver
+arduino = serial.Serial('/dev/ttyACM0', 9600) # ouvre le port série. Attention cela envoie un "reset" à l'arduino
+def httpserver():
+	PORT = 8000
+	Handler = http.server.SimpleHTTPRequestHandler
+	httpd = socketserver.TCPServer(("", PORT), Handler)
+	httpd.serve_forever()
+
+def write2munin():
+	while(True): #boucle pour la lecture
+		tempet = arduino.readline() #lit le port série
+		while(len(tempet) != 16): #vérifie les données reçues. Normalement, il doit lire 16 caractères.
+			tempet = arduino.readline() #Recommence la lecture s'il n'y a pas les 16 caractères.
+		tempet = tempet.decode("utf-8") #Transforme la chaine depuis 'bytes' vers 'utf-8' (python 3)
+		tempet = tempet.split('&') # Découpe la chaine sur les caractères '&'. 
+		Temp = int(tempet[0]) / 100
+		TempMoy = int(tempet[1]) / 100
+		TempObj = int(tempet[2]) / 100
+		f = open('temp2munin.txt','w')
+		f.write("Temp.value " + str(TempMoy) + "\n")
+#		f.write("TempObj.value " + str(TempObj) + "\n")
+		f.close()
+		time.sleep(15) #Attend 5 secondes avant de continuer
+
+threading.Thread(target = write2munin).start()
+threading.Thread(target = httpserver).start()
