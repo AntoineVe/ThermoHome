73774d1d3336534d8a0063ab88fc2c79e19a9abc
Antoine VAN-ELSTRAETE <antoine@van-elstraete.net>
2014-01-18 12:38:43 +0100

    Modification de la lecture série

--- software/python/exemple_read.py
+++ software/python/exemple_read.py
@@ -3,10 +3,10 @@ import serial #Importe la bibliothèque de communication série
 import time #Importe la bibliothèque pour gérer le temps
 arduino = serial.Serial('/dev/ttyACM0', 9600) # ouvre le port série. Attention cela envoie un "reset" à l'arduino
 a = 0
-while(a < 30): #boucle pour la lecture
+while(True): #boucle pour la lecture
     tempet = arduino.readline() #lit le port série
-    while(len(tempet) != 16): #vérifie les données reçues. Normalement, il doit lire 16 caractères.
-        tempet = arduino.readline() #Recommence la lecture s'il n'y a pas les 16 caractères.
+#    while(len(tempet) != 16): #vérifie les données reçues. Normalement, il doit lire 16 caractères.
+#        tempet = arduino.readline() #Recommence la lecture s'il n'y a pas les 16 caractères.
     tempet = tempet.decode("utf-8") #Transforme la chaine depuis 'bytes' vers 'utf-8' (python 3)
     tempet = tempet.split('&') # Découpe la chaine sur les caractères '&'. 
     Temp = int(tempet[0]) / 100
@@ -15,4 +15,4 @@ while(a < 30): #boucle pour la lecture
     print('La température actuelle est de ' + (str(Temp)) + '°C.')
     print('La température moyenne est de ' + (str(TempMoy)) + '°C.')
     print('L\'objectif de température est de ' + (str(TempObj)) + '°C.')
-    a = a+1 #boucle
+#    a = a+1 #boucle
