f6c1208ec5fd2c0f687abfaf860e89253b3aa761
Antoine VAN-ELSTRAETE <antoine@van-elstraete.net>
2014-10-24 16:10:07 +0200

    Ajout support sqlite pour programmation > 24h

--- software/serveur/ThermoCore.py
+++ software/serveur/ThermoCore.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import serial #Importe la bibliothèque de communication série
 import time #Importe la bibliothèque pour gérer le temps
-import threading #Multithreading
 import bottle #Serveur web Bottle
 import argparse #Parser d'arguments
+import sqlite3 #Importe la gestion des bases sqlite3
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--module", dest="modules", nargs="*", default="none", help="Liste de modules à ajouter")
+parser.add_argument("--base", dest="base", nargs=1, default="./thermo.db", type=str, help="Nom de la base sqlite")
 parser.add_argument("--port", dest="port", nargs=1, default="/dev/ttyACM0", type=str, help="Port de l'Arduino (/dev/ttyACM0)")
 parser.add_argument("--web-port", dest="webport", nargs=1, default="8000", type=int, help="Port pour le serveur web")
 parser.add_argument("--temp-only", dest="temponly", action="store", type=int, nargs="?", const=1, help="Affiche la température relevée et quitte")
@@ -47,10 +48,24 @@ if(args.temponly == 1):
     print(str(Temp) + "°C")
     quit()
 
+def checkdb(date):
+    global progjour
+    global db
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute("SELECT progjour FROM requested WHERE date = ?", (date,))
+    dbprog = c.fectchone())
+    if dbprog[0] != "":
+        progjour = dbprog[0]
+    conn.close()
+    return progjour
+
 speak2arduino() #Lance une première fois pour initialiser la variable Temp
 
 @bottle.get('/temp2munin.txt') #Sert le fichier pour munin
 def temp2munin():
+    today = time.strftime("%d-%m-%Y")
+    checkdb(today)
     speak2arduino()
     return bottle.template('Temp.value {{temp}}', temp=Temp)
 @bottle.get('/<filename:re:.*\.css>')
@@ -62,14 +77,14 @@ def stylesheets(filename):
 @bottle.route('/thermostat.html') #Page de monitoring et de programmation
 def thermostat():
     date = bottle.request.query.date
-    if(date == ""):
-        date = time.strftime("%d-%m-%Y")
+    checkdb(date)
     heure = time.strftime("%H:%M")
     speak2arduino()
     return bottle.template('thermostat', temp=Temp, obj=TempObj, heure=heure, prog=progjour, date=date)
 @bottle.route('/thermostat-prog.html', method='POST') #Page d'attente post programmation. Réaffiche la page de monitoring au bout de 8 secondes (temps de mise à jour du programme et de l'arduino)
 def do_thermostat():
     global progjour
+    global db
     def ogla(code):
         code = float(code)
         a = 80 + (code * 10 - 210) / 5
@@ -101,6 +116,12 @@ def do_thermostat():
     h23 = ogla(bottle.request.forms.get('prog_h23'))
     progjour = h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9 + h10 + h11 + h12 + h13 + h14 + h15 + h16 + h17 + h18 + h19 + h20 + h21 + h22 + h23
     date = bottle.request.forms.get('date')
+    dbstore = (date, progjour)
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute("INSERT OR REPLACE INTO requested VALUES(?,?)", (dbstore))
+    conn.commit()
+    conn.close()
     return bottle.template('thermostat_update.tpl', progjour=progjour, date=date)
 
 bottle.run(host='0.0.0.0', port=args.webport, debug=True) #commande lançant le serveur bottle
